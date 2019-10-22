aa8b87784bdf6c0ed9c027a953e53c2fd5aef02c
SImon <jolet.simon@yahoo.com>
2019-10-22 11:30:13 +0200

    Create components

--- CV-Angular/src/app/app.module.ts
+++ CV-Angular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './composants/home/home.component';
+import { NavBarComponent } from './composants/nav-bar/nav-bar.component';
+import { SidebarComponent } from './composants/sidebar/sidebar.component';
+import { BadgesComponent } from './composants/badges/badges.component';
+import { XpComponent } from './composants/xp/xp.component';
+import { ResumeComponent } from './composants/resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    SidebarComponent,
+    BadgesComponent,
+    XpComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
