5577bf2c85e89146fc2f45793bc0eb6c63d07432
SImon <jolet.simon@yahoo.com>
2019-10-22 14:45:28 +0200

    Add info home

--- CV-Angular/src/app/app-routing.module.ts
+++ CV-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./composants/home/home.component";
+import { NavBarComponent } from "./composants/nav-bar/nav-bar.component";
+import { SidebarComponent } from "./composants/sidebar/sidebar.component";
+import { BadgesComponent } from "./composants/badges/badges.component";
+import { XpComponent } from "./composants/xp/xp.component";
+import { ResumeComponent } from "./composants/resume/resume.component";
 
-const routes: Routes = [];
+const routes: Routes = [{ path: "", component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
