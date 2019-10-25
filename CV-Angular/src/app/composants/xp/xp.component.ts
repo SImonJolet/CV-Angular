c4eb003a37c48fd26ba7f550848c2c07d44952f3
SImon <jolet.simon@yahoo.com>
2019-10-25 16:45:50 +0200

    Update navbar in-out

--- CV-Angular/src/app/composants/xp/xp.component.ts
+++ CV-Angular/src/app/composants/xp/xp.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-xp',
-  templateUrl: './xp.component.html',
-  styleUrls: ['./xp.component.scss']
+  selector: "app-xp",
+  templateUrl: "./xp.component.html",
+  styleUrls: ["./xp.component.scss"]
 })
 export class XpComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
