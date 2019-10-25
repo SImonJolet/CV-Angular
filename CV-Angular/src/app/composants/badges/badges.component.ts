0b1f58598e9baefb38501918f364efbae018eb26
SImon <jolet.simon@yahoo.com>
2019-10-25 16:40:21 +0200

    navbar in badges

--- CV-Angular/src/app/composants/badges/badges.component.ts
+++ CV-Angular/src/app/composants/badges/badges.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-badges',
-  templateUrl: './badges.component.html',
-  styleUrls: ['./badges.component.scss']
+  selector: "app-badges",
+  templateUrl: "./badges.component.html",
+  styleUrls: ["./badges.component.scss"]
 })
 export class BadgesComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
